dfd9437577e58c719f48fb6e6cd7fae3544f53f0
TobiasReumann <tr064@hdm-stuttgart.de>
2017-05-01 19:46:32 +0200

    EigenschaftMapper um Methode inserEigenschaft() erweitert und getestet

--- src/de/hdm/gruppe1/Project4u/server/db/EigenschaftMapper.java
+++ src/de/hdm/gruppe1/Project4u/server/db/EigenschaftMapper.java
@@ -5,6 +5,9 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Vector;
+
+import com.ibm.icu.text.SimpleDateFormat;
+
 import de.hdm.gruppe1.Project4u.shared.bo.*;
 
 
@@ -39,6 +42,55 @@ public class EigenschaftMapper {
 		return eigenschaftMapper;
 	}
 	
-	
+	/**
+	 * Die Methode vergibt dem zu speichernden Eigenschafts-Objekts einen Primärschlüssel und legt es in 
+	 * der DB ab. Zudem aktualisiert sie das Änderungsdatum des zugehörigen Partnerprofil-Objekts
+	 * @param e Eigenschaft
+	 * @param p Partnerprofil
+	 * @return Eigenschaft
+	 * @author Tobias
+	 */
+	public Eigenschaft insertEigenschaft(Eigenschaft e, Partnerprofil p) {
+		Connection con = DBConnection.connection();
+
+		try {
+			Statement stmt = con.createStatement();
+
+			// Abfrage der größten bisher vergebnen <code>id</code>
+			ResultSet rs = stmt.executeQuery("SELECT MAX(id) AS maxid " + "FROM Eigenschaft ");
+
+			if (rs.next()) {
+				/*
+				 * Der bisher größte Primärschlüssel wird um 1 erhöht und dem
+				 * Eigenschaft-Objekt zugewiesen.
+				 */
+				e.setEigenschaftId(rs.getInt("maxid") + 1);
+
+				stmt = con.createStatement();
+
+				// Jetzt erst erfolgt die tatsÃ¤chliche EinfÃ¼geoperation
+				stmt.executeUpdate("INSERT INTO Eigenschaft (id, name, wert, partnerprofil_id) " + "VALUES ("
+						+ e.getEigenschaftId() + ",'" + e.getName() + "','" + e.getWert() + "','"
+						+ p.getPartnerprofilId() + "')");
+			}
+		} catch (SQLException s) {
+			s.printStackTrace();
+		}
+
+		/*
+		 * Im Anschluss an das Speichern des Eigenschaft-Objekts wird das
+		 * Änderungsdatum des verknüpften Partnerprofils aktualisiert.
+		 */
+		PartnerprofilMapper.partnerprofilMapper().updatePartnerprofil(p);
+		
+		
+		/*
+		 * Rückgabe, des evtl. korrigierten Partnerprofil-Objekts. P.S: Diese
+		 * Rückgabe ist nicht zwingend notwendig, da die Verweise auf das
+		 * bisherige Objekt auch auf das geänderte Objekt verweisen würden.
+		 */
+		return e;
+	}
+
 	
 }
