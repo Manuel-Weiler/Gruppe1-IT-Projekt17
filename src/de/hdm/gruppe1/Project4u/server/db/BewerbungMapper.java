0efcb4180f2349099e70286a142365fb6bd8897f
Dominik Sasse <ds153@hdm-stuttgart.de>
2017-05-09 22:19:12 +0200

    Bearbeitung Testklasse

--- src/de/hdm/gruppe1/Project4u/server/db/BewerbungMapper.java
+++ src/de/hdm/gruppe1/Project4u/server/db/BewerbungMapper.java
@@ -12,8 +12,8 @@ import de.hdm.gruppe1.Project4u.shared.bo.Bewerbung;
  * Mapper-Klasse, die <code>Nutzerprofil</code>-Objekte auf eine relationale
  * Datenbank abbildet. Hierzu wird eine Reihe von Methoden zur VerfÃ¼gung
  * gestellt, mit deren Hilfe z.B. Objekte gesucht, erzeugt, modifiziert und
- * gelÃ¶scht werden kÃ¶nnen. Das Mapping ist bidirektional. D.h., Objekte kÃ¶nnen
- * in DB-Strukturen und DB-Strukturen in Objekte umgewandelt werden.
+ * gelÃ¶scht werden kÃ¶nnen. Das Mapping ist bidirektional. D.h., Objekte
+ * kÃ¶nnen in DB-Strukturen und DB-Strukturen in Objekte umgewandelt werden.
  * 
  * 
  * @author Thies
@@ -21,7 +21,7 @@ import de.hdm.gruppe1.Project4u.shared.bo.Bewerbung;
  */
 
 public class BewerbungMapper {
-	
+
 	/**
 	 * Die Klasse NutzerprofilMapper wird nur einmal instantiiert. Man spricht
 	 * hierbei von einem sogenannten <b>Singleton</b>.
@@ -31,18 +31,18 @@ public class BewerbungMapper {
 	 * speichert die einzige Instanz dieser Klasse.
 	 * 
 	 */
-	
+
 	private static BewerbungMapper bewerbungMapper = null;
-	
+
 	/**
 	 * GeschÃ¼tzter Konstruktor - verhindert die MÃ¶glichkeit, mit
 	 * <code>new</code> neue Instanzen dieser Klasse zu erzeugen.
 	 */
-	
-	protected BewerbungMapper(){
-		
+
+	protected BewerbungMapper() {
+
 	}
-	
+
 	/**
 	 * Diese statische Methode kann aufgrufen werden durch
 	 * <code>NutzerprofilMapper.nutzerpofilMapper()</code>. Sie stellt die
@@ -56,46 +56,44 @@ public class BewerbungMapper {
 	 * 
 	 * @return bewerbungMapper <code>BewerbungMapper</code>-Objekt.
 	 */
-	public static BewerbungMapper bewerbungMapper(){
-		if(bewerbungMapper == null){
+	public static BewerbungMapper bewerbungMapper() {
+		if (bewerbungMapper == null) {
 			bewerbungMapper = new BewerbungMapper();
 		}
 		return bewerbungMapper;
 	}
-	
+
 	/**
-	 * Diese Methode bezieht ihre Informationen aus der
-	 * Project4uImpl und erstellt mit diesen eine neue
-	 * Bewerbung in der Datenbank.
+	 * Diese Methode bezieht ihre Informationen aus der Project4uImpl und
+	 * erstellt mit diesen eine neue Bewerbung in der Datenbank.
 	 * 
 	 * @param bewerbung
 	 * @return bewerbung
 	 */
-	public Bewerbung insert(Bewerbung bewerbung){
+	public Bewerbung insert(Bewerbung bewerbung) {
 		Connection con = DBConnection.connection();
-		
-		try{
+
+		try {
 			Statement stmt = con.createStatement();
-			
+
 			ResultSet rs = stmt.executeQuery("SELECT MAX(BewerbungID) AS maxid " + "FROM Bewerbung ");
-			
-			if(rs.next()){
-				bewerbung.setBewerbungID(rs.getInt("maxid")+1);
-				
-				stmt.executeUpdate("INSERT INTO Bewerbung (BewerbungID, Erstelldatum, Bewerbungstext)"
-						+ "VALUES (" + bewerbung.getBewerbungID() + "," + bewerbung.getErstelldatum() + "," 
+
+			if (rs.next()) {
+				bewerbung.setBewerbungID(rs.getInt("maxid") + 1);
+
+				stmt.executeUpdate("INSERT INTO Bewerbung (BewerbungID, Erstelldatum, Bewerbungstext)" + "VALUES ("
+						+ bewerbung.getBewerbungID() + "," + bewerbung.getErstelldatum() + ","
 						+ bewerbung.getBewerbungstext() + ")");
 			}
-		}catch (SQLException e){
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return bewerbung;
 	}
-	
+
 	/**
-	 * Diese Methode bezieht ihre Informationen aus der
-	 * Project4uImpl und ermöglicht es den Bewerbungstext
-	 * in der Datenbank zu ändern.
+	 * Diese Methode bezieht ihre Informationen aus der Project4uImpl und
+	 * ermöglicht es den Bewerbungstext in der Datenbank zu ändern.
 	 * 
 	 * @param bewerbung
 	 * @return bewerbung
@@ -103,55 +101,33 @@ public class BewerbungMapper {
 
 	public void updateBewerbung(Bewerbung bewerbung) {
 		Connection con = DBConnection.connection();
-		try{
+		try {
 			Statement stmt = con.createStatement();
-			stmt.executeUpdate("UPDATE Bewerbung " 	+ "WHERE BewerbungID =" + bewerbung.getBewerbungID());
+			stmt.executeUpdate("UPDATE Bewerbung " + "WHERE BewerbungID =" + bewerbung.getBewerbungID());
 			stmt = con.createStatement();
 			stmt.executeUpdate("UPDATE Bewerbung " + "SET Berbungstext=\"" + bewerbung.getBewerbungstext());
-		}catch (SQLException e2) {
+		} catch (SQLException e2) {
 			e2.printStackTrace();
 		}
-		
+
 	}
-	
+
 	/**
-	 * Diese Methode bezieht ihre Informationen aus der
-	 * Project4uImpl und ermöglicht es eine Bewerbung aus
-	 * der Datenbank zu löschen.
+	 * Diese Methode bezieht ihre Informationen aus der Project4uImpl und
+	 * ermöglicht es eine Bewerbung aus der Datenbank zu löschen.
 	 * 
 	 * @param bewerbung
 	 * @return bewerbung
 	 */
-	
+
 	public void deleteBewerbung(Bewerbung bewerbung) {
 		Connection con = DBConnection.connection();
 		try {
 			Statement stmt = con.createStatement();
-		      stmt.executeUpdate("DELETE FROM Bewerbung " + "WHERE BewerbungID=" + bewerbung.getBewerbungID());
-		    }
-		    catch (SQLException e2) {
-		      e2.printStackTrace();
-		    }
-		  }
-	
+			stmt.executeUpdate("DELETE FROM Bewerbung " + "WHERE BewerbungID=" + bewerbung.getBewerbungID());
+		} catch (SQLException e2) {
+			e2.printStackTrace();
+		}
+	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/de/hdm/gruppe1/Project4u/test/TestBewertungsmapper.java
+++ src/de/hdm/gruppe1/Project4u/test/TestBewertungsmapper.java
@@ -1,5 +1,21 @@
 package de.hdm.gruppe1.Project4u.test;
 
+import de.hdm.gruppe1.Project4u.server.db.BewertungMapper;
+import de.hdm.gruppe1.Project4u.shared.bo.Bewertung;
+
 public class TestBewertungsmapper {
+	
+	public static void main(String[] args) {
+		
+		Bewertung be = new Bewertung();
+		
+		//be.setBewertungID(1);
+		be.setBewertungspunkte(0);
+		be.setStellungnahme("blablub");
+		
+		BewertungMapper bem = BewertungMapper.bewertungMapper();
+		
+		bem.insert(be);
+	}
 
 }
