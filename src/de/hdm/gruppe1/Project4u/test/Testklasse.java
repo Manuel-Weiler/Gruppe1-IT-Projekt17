cd1326584eff15f9af323ff55e6a760e2c838d47
Georg <Georg@DESKTOP-O402QED>
2017-05-11 16:55:34 +0200

    An Testklasse die Mapper versucht zu testen

--- src/de/hdm/gruppe1/Project4u/test/Testklasse.java
+++ src/de/hdm/gruppe1/Project4u/test/Testklasse.java
@@ -1,57 +1,65 @@
-package de.hdm.gruppe1.Project4u.test;
-
-import java.util.Vector;
-
-import de.hdm.gruppe1.Project4u.server.db.AusschreibungMapper;
-import de.hdm.gruppe1.Project4u.server.db.EigenschaftMapper;
-import de.hdm.gruppe1.Project4u.server.db.PartnerprofilMapper;
-import de.hdm.gruppe1.Project4u.server.db.ProjektMapper;
-import de.hdm.gruppe1.Project4u.shared.bo.Ausschreibung;
-import de.hdm.gruppe1.Project4u.shared.bo.Eigenschaft;
-import de.hdm.gruppe1.Project4u.shared.bo.Partnerprofil;
-import de.hdm.gruppe1.Project4u.shared.bo.Projekt;
-
-public class Testklasse {
-	public static void main(String[] args) {
-		Ausschreibung a = new Ausschreibung (); 
-		
-		
-		a.setAusschreibungstext("Test Text");
-	//	a.setBewerbungsfrist(2017-06-15);
-		a.setBezeichnung("Test bezeichnung");
-	//	a.setErstellDatum(2017,05,12);
-		a.setID(1);
-		a.setNameProjektleiter("HS");
-		a.setPartnerprofilId(2);
-		a.setProjektId(3);
-		
-		Partnerprofil pa = new Partnerprofil();
-		Projekt pr = new Projekt ();
-		
-		
-		PartnerprofilMapper p = PartnerprofilMapper.partnerprofilMapper();
-		ProjektMapper l = ProjektMapper.projektMapper();
-		AusschreibungMapper am = AusschreibungMapper.ausschreibungMapper();
-	//	p=p.findById(0);
-		
-		am.insertAusschreibung(a, pa, pr);
-		/*
-		 * Vector<Eigenschaft> ev= new Vector<Eigenschaft>();
-		
-		 * ev=em.selectAllEigenschaftOfPartnerprofil(p);
-		 *
-		
-		for(Eigenschaft e: ev){
-			System.out.println(e.getName());
-			
-		} */
-		
-		
-		
-		
-		
-		
-	}
-		
-		
-}
+//package de.hdm.gruppe1.Project4u.test;
+//
+//import java.util.Date;
+//import java.util.Vector;
+//
+//import com.ibm.icu.text.SimpleDateFormat;
+//
+//import de.hdm.gruppe1.Project4u.server.db.AusschreibungMapper;
+//import de.hdm.gruppe1.Project4u.server.db.EigenschaftMapper;
+//import de.hdm.gruppe1.Project4u.server.db.PartnerprofilMapper;
+//import de.hdm.gruppe1.Project4u.server.db.ProjektMapper;
+//import de.hdm.gruppe1.Project4u.shared.bo.Ausschreibung;
+//import de.hdm.gruppe1.Project4u.shared.bo.Eigenschaft;
+//import de.hdm.gruppe1.Project4u.shared.bo.Partnerprofil;
+//import de.hdm.gruppe1.Project4u.shared.bo.Projekt;
+//
+//public class Testklasse {
+//	public static void main(String[] args) {
+//		
+//		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd");
+//		
+//	Date date = new Date ();
+//		
+//		Ausschreibung a = new Ausschreibung (); 
+//		
+//		
+//		a.setAusschreibungstext("Test Text");
+//		a.setBewerbungsfrist();
+//		a.setBezeichnung("Test bezeichnung");
+//		a.setErstellDatum("1996-10-10");
+//		a.setID(1);
+//		a.setNameProjektleiter("HS");
+//		a.setPartnerprofilId(2);
+//		a.setProjektId(3);
+//		
+//		Partnerprofil pa = new Partnerprofil();
+//		Projekt pr = new Projekt ();
+//		
+//		
+//		PartnerprofilMapper p = PartnerprofilMapper.partnerprofilMapper();
+//		ProjektMapper l = ProjektMapper.projektMapper();
+//		AusschreibungMapper am = AusschreibungMapper.ausschreibungMapper();
+//	//	p=p.findById(0);
+//		
+//		am.insertAusschreibung(a, pa, pr);
+//		/*
+//		 * Vector<Eigenschaft> ev= new Vector<Eigenschaft>();
+//		
+//		 * ev=em.selectAllEigenschaftOfPartnerprofil(p);
+//		 *
+//		
+//		for(Eigenschaft e: ev){
+//			System.out.println(e.getName());
+//			
+//		} */
+//		
+//		
+//		
+//		
+//		
+//		
+//	}
+//		
+//		
+//}
