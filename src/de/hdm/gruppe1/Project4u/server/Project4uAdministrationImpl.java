13127861fb7e41ee60e054bab0ec90b71bb33d0e
MaxBotta <max.botta@gmx.de>
2017-05-12 12:09:57 +0200

    Verbesserungen

--- src/de/hdm/gruppe1/Project4u/server/Project4uAdministrationImpl.java
+++ src/de/hdm/gruppe1/Project4u/server/Project4uAdministrationImpl.java
@@ -232,6 +232,11 @@ public class Project4uAdministrationImpl extends RemoteServiceServlet implements
 	}
 
 	public void deletePartnerprofil(Partnerprofil p) throws IllegalArgumentException {
+		
+		//Zugehörige EIgenschaften löschen
+		this.eigenschaftMapper.deleteEigenschaftOfPartnerprofil(p);
+		
+		//Partnerprofil löschen
 		partnerprofilMapper.deletePartnerprofil(p);
 	}
 
@@ -305,6 +310,7 @@ public class Project4uAdministrationImpl extends RemoteServiceServlet implements
 	 * @param p
 	 * @throws IllegalArgumentException
 	 */
+	
 	public void deleteAllEigenschaftOfPartnerprofil(Partnerprofil p)throws IllegalArgumentException{
 		eigenschaftMapper.deleteAllEigenschaftOfPartnerprofil(p);
 	}
