4efe82f1c03a4b8a74a7eb52a88bf3c7bffb134a
Ugur Bayrak <Ugur Bayrak ub014@hdm-stuttgart.de>
2017-05-16 23:20:16 +0200

    LoginInfo hinzugefügt

---
+++ src/de/hdm/gruppe1/Project4u/shared/LoginInfo.java
@@ -0,0 +1,62 @@
+package de.hdm.gruppe1.Project4u.shared;
+
+import java.io.Serializable;
+
+/**
+ * Diese Klasse enthält alle Login Informationen eines Nutzers.
+ * Dazu zählen der Login-Status, eine Login URL, eine Logout URL,
+ * eine Email Adresse sowie der Nickname des Nutzers.
+ * 
+ * @author GWT Tutorial
+ *
+ **/
+
+public class LoginInfo implements Serializable {
+private static final long serialVersionUID = 1L;
+
+	  private boolean loggedIn = false;
+	  private String loginUrl;
+	  private String logoutUrl;
+	  private String emailAddress;
+	  private String nickname;
+
+	  public boolean isLoggedIn() {
+	    return loggedIn;
+	  }
+
+	  public void setLoggedIn(boolean loggedIn) {
+	    this.loggedIn = loggedIn;
+	  }
+
+	  public String getLoginUrl() {
+	    return loginUrl;
+	  }
+
+	  public void setLoginUrl(String loginUrl) {
+	    this.loginUrl = loginUrl;
+	  }
+
+	  public String getLogoutUrl() {
+	    return logoutUrl;
+	  }
+
+	  public void setLogoutUrl(String logoutUrl) {
+	    this.logoutUrl = logoutUrl;
+	  }
+
+	  public String getEmailAddress() {
+	    return emailAddress;
+	  }
+
+	  public void setEmailAddress(String emailAddress) {
+	    this.emailAddress = emailAddress;
+	  }
+
+	  public String getNickname() {
+	    return nickname;
+	  }
+
+	  public void setNickname(String nickname) {
+	    this.nickname = nickname;
+	  }
+	}
