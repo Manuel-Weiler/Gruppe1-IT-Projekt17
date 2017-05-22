855b2ebf39c9cc64b42639b2576456e1067c94fb
Ugur Bayrak <Ugur Bayrak ub014@hdm-stuttgart.de>
2017-05-22 15:36:35 +0200

    Login korrektur

--- src/de/hdm/gruppe1/Project4u/shared/LoginServiceAsync.java
+++ src/de/hdm/gruppe1/Project4u/shared/LoginServiceAsync.java
@@ -2,9 +2,9 @@ package de.hdm.gruppe1.Project4u.shared;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
-import de.hdm.gruppe1.Project4u.shared.bo.Organisationseinheit;
+//import de.hdm.gruppe1.Project4u.shared.bo.Organisationseinheit;
 
 public interface LoginServiceAsync {
-	public void login(String requestUri, AsyncCallback<Organisationseinheit> async);
+	public void login(String requestUri, AsyncCallback<LoginInfo> async);
 
 }
